ee8970a4b1a7452177f3d3e88f22851a96e11970
Morfi Wifi <wifimorfi@gmail.com>
2017-11-08 23:00:41 +0330

    log in as in!

--- Kasrazhino/app/src/main/java/com/apps/kasrazhino/kasrazhino/MainActivity.java
+++ Kasrazhino/app/src/main/java/com/apps/kasrazhino/kasrazhino/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
         Animation animation = AnimationUtils.loadAnimation(this , R.anim.move_left);
 
