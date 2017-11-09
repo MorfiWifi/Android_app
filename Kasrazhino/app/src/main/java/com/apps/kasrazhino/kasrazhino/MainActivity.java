90a2f7408016d0d7b71180155e57c8af6e23852e
Morfi Wifi <wifimorfi@gmail.com>
2017-11-09 19:58:38 +0330

    -added jikmarams
    thanks to nik maram
    - abit error still!

--- Kasrazhino/app/src/main/java/com/apps/kasrazhino/kasrazhino/MainActivity.java
+++ Kasrazhino/app/src/main/java/com/apps/kasrazhino/kasrazhino/MainActivity.java
@@ -2,10 +2,8 @@ package com.apps.kasrazhino.kasrazhino;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.TranslateAnimation;
 
 public class MainActivity extends AppCompatActivity {
     static public int i = 0;
@@ -24,16 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (i== 0){
+       /* if (i== 0){
             i++;
             TranslateAnimation trans = new TranslateAnimation(0, 100, 0, 100);
             trans.setDuration(250);
             trans.setInterpolator(new AccelerateInterpolator(1.0f));
             findViewById(R.id.loginback).startAnimation(trans);
         }
-        else {
+        */
             super.onBackPressed();
-        }
+
 
     }
 }
---
+++ Kasrazhino/app/src/main/java/com/apps/kasrazhino/models/UserModel.java
@@ -0,0 +1,11 @@
+package com.apps.kasrazhino.models;
+
+/**
+ * User Model
+ */
+public class UserModel {
+    public String id;
+    public String email;
+    public String name;
+
+}
