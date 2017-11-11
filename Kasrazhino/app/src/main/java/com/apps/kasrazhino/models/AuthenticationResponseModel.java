441382de69a171e28b9078b2054b3bb7a0aa9d97
Morfi Wifi <wifimorfi@gmail.com>
2017-11-11 19:31:05 +0330

    !

--- Kasrazhino/app/src/main/java/com/apps/kasrazhino/models/AuthenticationResponseModel.java
+++
@@ -1,9 +0,0 @@
-package com.apps.kasrazhino.models;
-
-/**
- * Authentication response Model used as response model in sign in and sign up request
- */
-public class AuthenticationResponseModel {
-    public TokenModel token;
-    public UserModel user_profile;
-}
